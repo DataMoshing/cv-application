d851618ca08349249dbb7860dbe91c4eb9d914a1
DataMoshing <55223157+DataMoshing@users.noreply.github.com>
2023-10-23 16:09:51 -0700

    Created component for work experience

---
+++ src/components/ExperienceInfo.jsx
@@ -0,0 +1,17 @@
+export default function WorkExperience() {
+    return (
+        <form>
+            <h1>Work Experience</h1>
+            <h3>Company</h3>
+            <input type="text" placeholder="Vercel" />
+            <h3>Title / Position</h3>
+            <input type="text" placeholder="Software Engineer" />
+            <h3>Start date</h3>
+            <input type="text" placeholder="mm / yyyy" />
+            <h3>End date</h3>
+            <input type="text" placeholder="mm / yyyy" />
+            <h3>Responsibilities</h3>
+            <textarea></textarea>
+        </form>
+    )
+}
