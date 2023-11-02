8ec8470587032a6949f083020f00868c364a2242
DataMoshing <55223157+DataMoshing@users.noreply.github.com>
2023-11-02 00:05:20 -0700

    Moved Contact component to separate file

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -1,61 +1,37 @@
-import { useState } from "react"
-import EducationInfo from "./EducationInfo"
-import WorkExperience from "./ExperienceInfo"
-
-export default function ContactInfo() {
-    const [info, setInfo] = useState({ firstName: "", lastName: "", email: "", phoneNumber: "", degree: "" })
-
-    function changeInfo(e) {
-        setInfo({ ...info, [e.target.name]: e.target.value })
-    }
-
+export default function ContactInfo({ firstName, onFirstNameChange, lastName, onLastNameChange, email, onEmailChange, phoneNumber, onPhoneNumberChange }) {
     return (
-        <>
-            <div className="form-container">
-                <form>
-                    <label htmlFor="first-name">
-                        <span className="label-text">First name: </span>
-                    </label>
-                    <input type="text" className="firstName"
-                        name="firstName"
-                        value={info.firstName}
-                        onChange={changeInfo}
-                    />
-                    <label htmlFor="last-name">
-                        <span className="label-text">Last name: </span>
-                    </label>
-                    <input type="text" className="lastName" name="lastName"
-                        value={info.lastName}
-                        onChange={changeInfo}
-                    />
-                    <label htmlFor="email">
-                        <span className="label-text">Email: </span>
-                    </label>
-                    <input type="email" className="email" name="email"
-                        value={info.email}
-                        onChange={changeInfo} />
-                    <label htmlFor="phone-number">
-                        <span className="label-text">Phone number: </span>
-                    </label>
-                    <input type="tel"
-                        className="phone-number"
-                        name="phoneNumber"
-                        value={info.phoneNumber}
-                        onChange={changeInfo}
-                    />
-                </form>
-                <EducationInfo degree={info.degree} onDegreeChange={changeInfo}
-                    name="degree" />
-                <WorkExperience />
-                <div className="resume-container">
-                    <h1 className="resume-name">{info.firstName}{" "}{info.lastName}</h1>
-                    <span>{info.email}</span>
-                    <br></br>
-                    <br></br>
-                    <span>{info.phoneNumber}</span>
-                    <p>{info.degree}</p>
-                </div>
-            </div>
-        </>
+        <form>
+            <h1>Contact</h1>
+            <label htmlFor="first-name">
+                <span className="label-text">First name: </span>
+            </label>
+            <input type="text" className="firstName"
+                name="firstName"
+                value={firstName}
+                onChange={onFirstNameChange}
+            />
+            <label htmlFor="last-name">
+                <span className="label-text">Last name: </span>
+            </label>
+            <input type="text" className="lastName" name="lastName"
+                value={lastName}
+                onChange={onLastNameChange}
+            />
+            <label htmlFor="email">
+                <span className="label-text">Email: </span>
+            </label>
+            <input type="email" className="email" name="email"
+                value={email}
+                onChange={onEmailChange} />
+            <label htmlFor="phone-number">
+                <span className="label-text">Phone number: </span>
+            </label>
+            <input type="tel"
+                className="phone-number"
+                name="phoneNumber"
+                value={phoneNumber}
+                onChange={onPhoneNumberChange}
+            />
+        </form>
     )
 }
