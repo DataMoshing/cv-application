de6000782c73b6ced57ad15a29aa596e94a95226
DataMoshing <55223157+DataMoshing@users.noreply.github.com>
2023-11-03 00:27:50 -0700

    Created labels for education component + props

--- src/components/EducationInfo.jsx
+++ src/components/EducationInfo.jsx
@@ -3,21 +3,41 @@ export default function EducationInfo({ degree, onDegreeChange, college, onColle
         <>
             <form>
                 <h1>Education</h1>
-                <span className="label-text">Degree: </span>
+                <label htmlFor="degree">
+                    <span className="label-text">Degree: </span>
+                </label>
                 <input type="text"
-                    value={degree} onChange={onDegreeChange} name="degree"
+                    value={degree}
+                    onChange={onDegreeChange}
+                    id="degree"
+                    name="degree"
                 />
-                <span className="label-text">College / University: </span>
+                <label htmlFor="college">
+                    <span className="label-text">College / University: </span>
+                </label>
                 <input type="text"
-                    value={college} onChange={onCollegeChange} name="college"
+                    value={college}
+                    onChange={onCollegeChange}
+                    id="college"
+                    name="college"
                 />
-                <span className="label-text">Start date:  </span>
+                <label htmlFor="ed-start-date">
+                    <span className="label-text">Start date:  </span>
+                </label>
                 <input type="text" placeholder="mm / yyyy"
-                    value={edStartDate} onChange={onEdStartChange} name="edStartDate"
+                    value={edStartDate}
+                    onChange={onEdStartChange}
+                    id="ed-start-date"
+                    name="edStartDate"
                 />
-                <span className="label-text">End date:  </span>
+                <label htmlFor="ed-end-date">
+                    <span className="label-text">End date:  </span>
+                </label>
                 <input type="text" placeholder="mm / yyyy"
-                    value={edEndDate} onChange={onEdEndChange} name="edEndDate" />
+                    value={edEndDate}
+                    id="ed-end-date"
+                    onChange={onEdEndChange}
+                    name="edEndDate" />
             </form>
         </>
     )
