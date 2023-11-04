28badca8b197f981f3c96eb1460f64a9febd14ca
DataMoshing <55223157+DataMoshing@users.noreply.github.com>
2023-11-03 20:20:06 -0700

    Added ids to input elements

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -5,7 +5,7 @@ export default function ContactInfo({ firstName, onFirstNameChange, lastName, on
             <label htmlFor="first-name">
                 <span className="label-text">First name: </span>
             </label>
-            <input type="text" className="firstName"
+            <input type="text" className="firstName" id="first-name"
                 name="firstName"
                 value={firstName}
                 onChange={onFirstNameChange}
@@ -13,14 +13,18 @@ export default function ContactInfo({ firstName, onFirstNameChange, lastName, on
             <label htmlFor="last-name">
                 <span className="label-text">Last name: </span>
             </label>
-            <input type="text" className="lastName" name="lastName"
+            <input type="text" className="lastName"
+                id="last-name"
+                name="lastName"
                 value={lastName}
                 onChange={onLastNameChange}
             />
             <label htmlFor="email">
                 <span className="label-text">Email: </span>
             </label>
-            <input type="email" className="email" name="email"
+            <input type="email" className="email"
+                id="email"
+                name="email"
                 value={email}
                 onChange={onEmailChange} />
             <label htmlFor="phone-number">
@@ -28,6 +32,7 @@ export default function ContactInfo({ firstName, onFirstNameChange, lastName, on
             </label>
             <input type="tel"
                 className="phone-number"
+                id="phone-number"
                 name="phoneNumber"
                 value={phoneNumber}
                 onChange={onPhoneNumberChange}
